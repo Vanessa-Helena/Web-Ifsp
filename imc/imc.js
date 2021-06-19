f5e44d75650767c7b3b35a366f3a1d75c111c655
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-06-19 12:40:51 -0300

    primeiro commit

---
+++ imc/imc.js
@@ -0,0 +1,26 @@
+var peso 
+var altura 
+
+var imc = peso * altura **2;
+
+switch (imc) {
+    case value: imc <= 18,5
+        print('Abaixo do peso')
+        break;
+    case value: imc > 18,5 && imc <= 25
+        print('Peso normal')
+        break;
+    case value: imc > 25 && imc <= 30
+        print('Acima do peso')
+        break;
+    case value: imc > 30
+        print('Obeso')
+        break;
+}
+
+function imc (
+    inPeso = document.querySelector('#inPeso'),
+    inAltura = document.querySelector('#inAltura')
+
+
+)
