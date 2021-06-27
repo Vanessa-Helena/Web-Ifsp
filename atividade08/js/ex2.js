dcde5a9fab33022e040c172afadb7179bfe79599
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-06-27 00:03:39 -0300

    Inclusao ext2

--- atividade08/js/ex2.js
+++ atividade08/js/ex2.js
@@ -1,49 +1,36 @@
 /*2. Altere a aplicação da questão 1 para fazer conversões de graus Celsius para Kelvin, e vice-versa. A página HTML não deve conter elementos em seu código, todos os elementos devem ser criados em JavaScript.*/
 
-var elemento_input = document.createElement('input');;
-var elemento_h1 = document.createElement('h1');
 var elemento_pai = document.body;
-var text_h1 = document.createElement('p');
-var text_h3 = document.createElement('h3');
-var elemento_button = document.createElement('button')
+var titulo = document.createElement('h1');
+var texto = document.createElement('p');
+var input = document.createElement('input');
+var botao = document.createElement("button");
+var result = document.createElement("h3");
 
-elemento_h1.innerHTML = "Conversor de Graus";
-document.body.appendChild(elemento_h1);
+titulo.innerHTML = "Conversor de Graus";
+document.body.appendChild(titulo);
 
-document.body.appendChild(elemento_h1);
+texto.innerHTML = "Digite quantos graus: ";
+document.body.appendChild(texto); 
 
-text_h1.innerHTML = "Insira um numero";
-document.body.appendChild(text_h1);
+document.body.appendChild(input);
 
-document.body.appendChild(elemento_input);
+botao.innerHTML = "Converter";
+document.body.appendChild(botao);
 
-elemento_button.innerHTML = "Converter";
-document.body.appendChild(elemento_button);
+document.body.appendChild(result);
 
-document.body.appendChild(text_h3);
+function converteKelvin(){
 
-function convertKelvin(){
-    inp = document.querySelector('input')
-    outConvertido = document.querySelector('h3')
+    inp = document.querySelector("input");
+    outResult = document.querySelector("h3");
 
     valor = Number(inp.value);
 
-    var conversao = valor + 273.15;
+    var convertido = valor + 273.15;
 
-    outConvertido.textContent = "O grau em Kelvin e: " + conversao;
+    outResult.textContent = "Convertido fica assim: " + convertido;
 }
 
-btConversao = document.querySelector('button');
-btConversao.addEventListener('click', convertKelvin);
-
-
-
-
-
-
-
-
-
-
-
-
+var btBotao = document.querySelector("button");
+btBbotao = addEventListener("click", converteKelvin);
