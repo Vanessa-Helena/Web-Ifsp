2fe4966877053508a17c8394118a48fcb9883f32
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-10-23 19:30:31 -0300

    api rest

--- webII/ex_templating/index.js
+++ webII/ex_templating/index.js
@@ -2,13 +2,32 @@ const express = require('express')
 const app = express();
 
 const path = require('path');
+const dadosReddit = require('./dados.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+
 app.get('/', (req, res)=>{
     const num = Math.floor(Math.random()*10)
-    res.render('home', {num});
+    const val = "Texto de Servidor!"
+    res.render('home', {num, val});
+});
+
+
+app.get('/r/:subreddit', (req, res)=>{
+    const {subreddit} = req.params;
+    const dados = dadosReddit['subreddit'];
+
+    if(dados){
+        res.render('subreddit', {...dados}); //esta pegando todos os dados pegos na variavel dados
+    }else {
+        res.send('<h1>URL Não Encontrada</h1>')
+    }
 });
 
 app.listen(80, ()=>{
